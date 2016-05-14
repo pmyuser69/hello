796eb4ea00af1bf719c385b7ecd115d51d1f5f89
Patrick <pmyuser@yahoo.fr>
2016-05-14 10:46:34 +0200

    RNG

--- src/main/scala/hw.scala
+++ src/main/scala/hw.scala
@@ -1,3 +1,22 @@
-object Hi {
-  def main(args: Array[String]) = println("Hi Guys! 2")
+trait RNG {
+  def nextInt: (Int, RNG)
 }
+
+object RNG {
+  def simple(seed: Long): RNG = new RNG {
+    def nextInt = {
+      val seed2 = (seed * 0x5DEECE66DL + 0xBL) & ((1L << 48) - 1)
+      ((seed2 >>> 16).asInstanceOf[Int], simple(seed2))
+    }
+  }
+}
+
+object Hi {
+  def main(args: Array[String]) = {
+    println("Hi Guys! 2")
+    var r: RNG = RNG.simple(10)
+    println(r.nextInt._1)
+  }
+}
+
+
